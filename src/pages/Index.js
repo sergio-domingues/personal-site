5d179f05011379d476884aaa019c3f84dd48cc38
sergio.domingues <sergio.domingues@jscrambler.com>
2021-01-19 15:03:37 +0000

    Update site about page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -12,8 +12,8 @@ const Index = () => (
         <div className="title">
           <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
           <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+            A responsive, statically-generated,
+            react Single-page application written with modern Javascript.
           </p>
         </div>
       </header>
@@ -21,7 +21,6 @@ const Index = () => (
         or you can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
     </article>
